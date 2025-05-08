9861d69f45b56fba8332ec76f3f1131aa353c3d5
Gasser <gasser@localhost.localdomain>
2025-05-08 23:49:56 +0300

    first V

---
+++ calc.js
@@ -0,0 +1,75 @@
+const symbols = ['+', '-', '*', '/' ,'%', 'C', '=', '←']; // أضفنا السهم إلى الرموز
+const symbolMap = {
+    '÷': '/',
+    '×': '*',
+    '−': '-',
+    '+': '+',
+    '=': '=',
+    'C': 'C'
+};
+
+
+function  clear(){
+    console.log("Cleared");
+    document.querySelector('.screen').innerText = 0;
+}
+
+function addToDisplay(value){
+    value = symbolMap[value] || value;
+    const screen = document.querySelector('.screen');
+if (screen.innerText === "0" && !symbols.includes(value)){
+    screen.innerText = value;
+} else {
+    screen.innerText += value;
+}
+}
+
+
+function backspace(){
+    const screen = document.querySelector('.screen');
+    if (screen.innerText.length > 1) {
+        screen.innerText = screen.innerText.slice(0, -1);
+    } else {
+        screen.innerText = '0';
+    }
+}
+calculate(){
+    const screen = document.querySelector('.screen');
+    let exrpression = screen.innerText;
+    exrpression = exrpression.replace(/×/g, '*').replace(/÷/g, '/').replace(/−/g, '-');
+    try {
+        const result = eval(exrpression);
+        screen.innerText = result;
+    }
+
+}
+
+function buttonClick(value){
+    if (isNaN(value) && symbols.includes(value)){
+        if (value === "C"){
+            clear();
+        } else if (value === "="){
+            console.log("Calculating");
+            calculate();
+        } else if (value === "←") {
+            backspace();
+        } else {
+            addToDisplay(value);
+        }
+    }
+    else {
+        addToDisplay(value);
+    }
+}
+
+
+function init(){
+    document.querySelector('.calc-buttons').addEventListener("click", function (event){
+        if (event.target.tagName === "BUTTON") {
+            buttonClick(event.target.innerText);
+        }
+    });
+}
+
+init();
+
